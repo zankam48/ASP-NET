3ba85c299ab5d40e1a2027723e452ff0a00b2549
hikamizan <hikamizan771@gmail.com>
2025-02-26 13:45:20 +0700

    create DBSet

--- WebApplication1/Data/GameStoreContext.cs
+++ WebApplication1/Data/GameStoreContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entities;
 
 namespace WebApplication1.Data;
 
 public class GameStoreContext(DbContextOptions<GameStoreContext> options) 
     : DbContext(options) 
 {
+    public DbSet<Game> Games => Set<Game>();
 
+    public DbSet<Genre> Genres => Set<Genre>();
 }
